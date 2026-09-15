914cabf539e6755a67bd5619704dcfeec4a9a013
Gustavo Nienov <henriquenienov@gmail.com>
2026-09-15 19:31:03 -0300

    inicia models users

--- backend/app/db/__init__.py
+++ backend/app/db/__init__.py
@@ -1,1 +1,5 @@
 """Recursos de conexao e configuracao do banco de dados."""
+
+from app.db.base import Base
+from app.modules.auth.models import Usuario, Analista
+from app.modules.investors.models import Investidor
